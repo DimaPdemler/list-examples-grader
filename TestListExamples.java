e6a39dc2d2bc463709b81d6bc3b43eca99321a2b
DimaPdemler <Dimademler@gmail.com>
2022-11-10 11:48:36 -0800

    tesr

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,5 +2,20 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestListExamples {
-  // Write your grading tests here!
+
+
+
+
+  public boolean checkString(String string1){
+    if (string1.length()%2==0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  @Test
+  public void randomtest(){
+    // assertEquals(checkString("fish"), checkString("fooood"));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -2,6 +2,44 @@
 
 set -e
 
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+
+
+
+
+
+
+
 rm -rf student-submission
 git clone $1 student-submission
+
+
+
+cp TestListExamples.java  ./student-submission/
+ls student-submission/
+if [ -e "student-submission/ListExamples.java" ]
+then 
+   echo found
+else
+    echo not found
+    exit
+
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/TestListExamples.java student-submission/ListExamples.java
+
+cd  student-submission/
+
+
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
+
+
+
+
+
+
 
