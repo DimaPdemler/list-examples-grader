05e9477595b2471b717380d147195dcdef4d06c3
DimaPdemler <Dimademler@gmail.com>
2022-11-17 18:12:44 -0800

    f

--- grade.sh
+++ grade.sh
@@ -11,29 +11,29 @@ CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 
 
-rm -rf student-submission
-git clone $1 student-submission
+# rm -rf student-submission
+# git clone $1 student-submission
 
 
 
-cp TestListExamples.java  ./student-submission/
-ls student-submission/
-if [ -e "student-submission/ListExamples.java" ]
-then 
-   echo found
-else
-    echo not found
-    exit
+# cp TestListExamples.java  ./student-submission/
+# ls student-submission/
+# if [ -e "student-submission/ListExamples.java" ]
+# then 
+#    echo found
+# else
+#     echo not found
+#     exit
 
-fi
+# fi
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/TestListExamples.java student-submission/ListExamples.java
+# javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/TestListExamples.java student-submission/ListExamples.java
 
-cd  student-submission/
+# cd  student-submission/
 
 
 
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+# java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 
 
