cbc33dbb087d6f4c3eb5bc811b73b557818629e1
DimaPdemler <Dimademler@gmail.com>
2022-11-23 22:57:36 -0800

    lab report 5 update new patch notes

--- grade.sh
+++ grade.sh
@@ -1,17 +1,21 @@
-# Create your grading script here
+# Inspired by code from this the repository https://github.com/kNelakonda/list-examples-grader/blob/main/grade.sh
 FSCORE=0
 MAXSCORE=100
 
 function score_message() {
     printf "Student recieved $1/$2 \n"
+    
     exit 0
 }
 
 rm -rf student-submission
+
 git clone $1 student-submission
+
 echo 'Finished cloning'
 
 cp TestListExamples.java ./student-submission/
+
 
 if [ -f ./student-submission/ListExamples.java ]; then
     echo found
@@ -40,7 +44,7 @@ if [[ $? -eq 0 ]]; then
     cat output.txt
     FSCORE=$((FSCORE + 70))
 else
-    less output.txt
+    cat output.txt
     t=$(grep -i "Tests run" output.txt)
     tests=$(echo $t | cut -d ' ' -f 3 | cut -d ',' -f 1)
     failed=$(echo $t | cut -d ' ' -f 5)
