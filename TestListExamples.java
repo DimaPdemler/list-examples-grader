f375e2e749152cadfb1c6218e86b0541e2831d8c
DimaPdemler <Dimademler@gmail.com>
2022-11-23 22:03:00 -0800

    j

--- TestListExamples.java
+++ TestListExamples.java
@@ -5,35 +5,52 @@ import java.util.List;
 import org.junit.Test;
 
 public class TestListExamples {
-
-
+  
 
 
 
   @Test
-  public void randomtest(){
+  public void filterCorrectly() {
     List<String> i = new ArrayList<String>();
-    List<String> flist = new ArrayList<String>();
-
-
+    List<String> endList = new ArrayList<String>();
+    i.add("1Hello");
+    i.add("We");
+    i.add("1Are2");
+    i.add("Here");
     StringChecker wc = new ObjChecker();
-    i.add("hi");
-    i.add("fish");
-    i.add("meats");
-    i.add("ree");
-    i.add("me");
-    // ListExamples.filter(i, wc);
-    flist.add("hi");
-    flist.add("fish");
-    flist.add("me");
-    
-    assertEquals(flist,ListExamples.filter(i,wc));
-
-
-
-    
-    // assertEquals(checkString("fish"), checkString("fooood"));
+    ListExamples.filter(i, wc);
+    endList.add("1Hello");
+    endList.add("We");
+    endList.add("Here");
+    assertEquals(endList,ListExamples.filter(i,wc));
   }
+
+  @Test
+  public void lengthtest(){
+    List<String> list1 = new ArrayList<String>();
+      List<String> list2 = new ArrayList<String>();
+      list1.add("food1");
+      list1.add("foo2");
+      list1.add("food3");
+      list1.add("foo4");
+      list1.add("food5");
+      list1.add("food6");
+
+      StringChecker wc2 = new ObjChecker();
+     
+      
+     
+      // List<String> list3 = ListExamples.merge(list1, list2);
+      
+      assertEquals(2,  ListExamples.filter(list1, wc2).size());
+  }
+ 
+
+
+
+
+
+  // Write your grading tests here!
 }
 
 class ObjChecker implements StringChecker{
--- grade.sh
+++ grade.sh
@@ -1,92 +1,60 @@
 # Create your grading script here
-
 FSCORE=0
 MAXSCORE=100
 
-# This was inspired by the repository I peer reviewed. Its link is: https://github.com/kNelakonda/list-examples-grader
 function score_message(){
     printf "Student recieved $1/$2 \n"
     exit 0
 }
 
 
-set -e
-
-CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
-
-
-
-
-
-
 rm -rf student-submission
 git clone $1 student-submission
-
-
+echo 'Finished cloning'
 
 cp TestListExamples.java  ./student-submission/
-cp ListExamples.java  ./student-submission/
 
-ls student-submission/
-if [ -e "student-submission/ListExamples.java" ]
-then 
+if [ -f ./student-submission/ListExamples.java ]
+then
    echo found
    FSCORE=$((FSCORE + 10))
 else
-    echo not found
-    exit
-
-fi
-
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/TestListExamples.java student-submission/ListExamples.java 
-
-# java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output1.txt
-
-
-
-printf "\n---Running Java files now---\n"
-if [[ $? -ne 0 ]]
-then
+    echo not found java file
     score_message $FSCORE $MAXSCORE
 fi
 
-FSCORE=$((FSCORE + 10))
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/TestListExamples.java student-submission/ListExamples.java
 
+if [[ $? -ne 0 ]]
+then
+    printf "Compiler did not work\n"
+    score_message $FSCORE $MAXSCORE
+fi
 
+FSCORE=$((FSCORE + 20))
 
 cd  student-submission/
 
+printf "_________running java now__________\n"
 
-
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples | tee -a output.txt
-
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 
 if [[ $? -eq 0 ]]
 then
-    FSCORE=$((FSCORE + 80))
-
+    cat output.txt
+    FSCORE=$((FSCORE + 70))
 else
-
+    less output.txt
     t=$(grep -i "Tests run" output.txt)
 	tests=$(echo $t | cut -d ' ' -f 3 | cut -d ',' -f 1)
 	failed=$(echo $t | cut -d ' ' -f 5)
 
     echo $((tests - failed))/$tests tests passed
 
-	FSCORE=$((FSCORE + 15*(tests - failed)))
+	FSCORE=$((FSCORE + 35*(tests - failed)))
 
 fi
 
-
 score_message $FSCORE $MAXSCORE
-
-
-
-
-
-
-
-
-
 
 
